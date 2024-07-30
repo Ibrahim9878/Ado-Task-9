60c674e4c7e9616eb9daf6d075c5e27e43062989
Talib_ip88 <Talib_ip88@STHQ0128-15>
2024-07-30 13:38:17 +0400

    Add project files.

---
+++ Ado Task 9/IAuthorRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Ado_Task_9;
+
+public interface IAuthorRepository
+{
+    Author Add(Author author);
+    void AddAuthors(List<Author> authors);
+    void Remove(int id);
+    void RemoveByIdArray(int[] ids);
+    void RemoveRange(int start, int end);
+    Author Update(Author author);
+    void UpdateAuthors(List<Author> authors);
+    IEnumerable<Author> GetAll();
+    Author GetById(int id);
+}
---
+++ Ado Task 9/Program.cs
@@ -0,0 +1,28 @@
+﻿using Ado_Task_9;
+using System.Data.SqlClient;
+
+IAuthorRepository repo = new AuthorRepository(new SqlConnection(),
+    @"Server=(localdb)\MSSQLLocalDB;Database=Library;Integrated Security=SSPI;");
+
+//var author = repo.Add(new Author() { FirstName = "Neil", LastName = "Gaiman" });
+//var author2 = repo.Add(new Author() { FirstName = "Zahid", LastName = "Xelil" });
+//Console.WriteLine(author2);
+
+//Console.WriteLine(repo.GetById(13));
+//repo.Remove(18);
+//repo.Update(new Author { Id = 16, FirstName = "Nadir", LastName = "Zaman" });
+//repo.AddAuthors(new List<Author>()
+//{
+//    new Author() {FirstName = "Huseyn",LastName = "Memmedov"},
+//    new Author() {FirstName = "Maga",LastName = "Fetullayev"},
+//    new Author() {FirstName = "Ibrahim",LastName = "Talibli"}
+//});
+//repo.RemoveByIdArray([15,16,17]);
+//repo.RemoveRange(18, 20);
+repo.UpdateAuthors(new List<Author>()
+{
+    new Author() {Id = 1,FirstName = "Huseyn",LastName = "Memmedov"},
+    new Author() {Id = 2,FirstName = "Maga",LastName = "Fetullayev"},
+    new Author() {Id = 3,FirstName = "Ibrahim",LastName = "Talibli"}
+});
+repo.GetAll().ToList().ForEach(Console.WriteLine);
